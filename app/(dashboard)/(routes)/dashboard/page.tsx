685e45f4e4450b390c442bcd9cbe458e59ca551b
Jeffrey Nibbs <djcountry100@gmail.com>
2023-08-17 18:00:59 -0700

    half set up

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+export default function DashboardPage() {
+  return (
+    <>
+      <p>(protected)</p>
+      <Button variant="destructive" size="lg" className="m-5">
+        Click me
+      </Button>
+    </>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+export default function LandingPage() {
+  return (
+    <>
+      <p>(unprotected)</p>
+      <Button variant="default" size="lg" className="m-5">
+        Click me
+      </Button>
+    </>
+  )
+}
